f67578292ad6ed8e7e864ddef2e0e0ebb040267b
bogdantatu <bogdanntatu@yahoo.com>
2021-04-28 00:58:53 +0300

    First Commit

---
+++ backend/Database/db-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "database": "postgres",
+    "dbUser": "postgres",
+    "dbPassword": "letmein20@",
+    "host": "localhost",
+    "port": 5000,
+    "dialect": "postgres"
+} 

545a39c5514b0b7dc3f59beb161c39cbafe9e095
bogdantatu <bogdanntatu@yahoo.com>
2021-05-18 16:51:20 +0300

    Register Done

--- frontend/src/components/Authentication/Register/Register.js
+++ frontend/src/components/Authentication/Register/Register.js
@@ -4,14 +4,25 @@ import classes from './Register.module.css';
 import Button from '../../UI/Button/Button'
 
 const register = (props) => (
-    <div className={classes.Register}>
-        <h1>Create your account</h1>
-        <span>Use your email for signing up</span>
-        <form className={classes.Form}>
-            <input className={classes.Input} type="email" name="email" placeholder="Email"/>        
-            <input className={classes.Input} type="password" name="password" placeholder="Password"/>            
-        <Button>Register</Button>    
-        </form>
+    <div className={classes.Section}>
+   
+        <div className={classes.RegisterRight}>
+            <div className={classes.RegisterRightWrapper}>
+                <h1>Register</h1>
+                <form className={classes.Form}>
+                    <input className={classes.Input} type="email" name="email" placeholder="Email"/>        
+                    <input className={classes.Input} type="password" name="password" placeholder="Password"/>            
+                <Button>Register</Button>
+                </form>
+            </div>
+        </div>
+        <div className={classes.RegisterLeft}>
+            <div className={classes.RegisterLeftWrapper}>
+                <p>logo</p>
+                <h1>Denumire Aplicatie</h1>
+                <p>Do you already have an account?</p>
+            </div>
+        </div>
     </div>
 );
 export default register;
--- frontend/src/containers/Authentication/Authentication.js
+++ frontend/src/containers/Authentication/Authentication.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux/Aux';
 import Login from '../../components/Authentication/Login/Login'
-// import Register from '../../components/Authentication/Register/Register'
+import Register from '../../components/Authentication/Register/Register'
 
-import classes from './Authentication.module.css'
+
 
 
 class Authentication extends Component{
     render() {
         return(
             <Aux>
-                <div className={classes.Authentication}>
+                <div>
                     <Login />
-                    {/* <Register /> */}
+                    <Register />
                 </div>
             </Aux>
         );
